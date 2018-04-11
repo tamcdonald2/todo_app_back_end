cb15477d67a8d58a61a859c74f18de50e64a531e
Trinity M <tamcdonald2@cps.edu>
2018-04-11 14:06:42 +0000

    Add the RCAV flows for the new and create actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,14 +2,7 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        todo_id = '2'
-  if todo_id == '1'
-    @todo_description = "Make the curriculum"
-    @todo_pomodoro_estimate = 4
-  elsif todo_id == '2'
-    @todo_description = "Buy workshop supplies"
-    @todo_pomodoro_estimate = 3
-  end
+    @todo = Todo.find_by_id(params[:id])
 
     end
 end
