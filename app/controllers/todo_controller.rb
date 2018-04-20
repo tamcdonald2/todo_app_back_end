cf74502c37bab1f79d8aefb812e4de2eac81a1dd
Trinity M <tamcdonald2@cps.edu>
2018-04-20 19:16:13 +0000

    Add root route

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,4 +5,31 @@ class TodoController < ApplicationController
     @todo = Todo.find_by_id(params[:id])
 
     end
+    def new
+    end
+    def create
+  t = Todo.new
+  t.description = params['description']
+  t.pomodoro_estimate = params['pomodoro_estimate']
+  t.save
+  redirect_to "/todo/show/#{ t.id }"
+    end
+    def edit
+  @todo = Todo.find_by_id(params[:id])
+    end
+    def update
+  t = Todo.find_by_id(params['id'])
+  t.description = params['description']
+  t.pomodoro_estimate = params['pomodoro_estimate']
+  t.save
+  redirect_to "/todo/show/#{t.id}"
+    end
+    def destroy
+  t = Todo.find_by_id(params[:id])
+  t.destroy
+  redirect_to "/todo/index"
+    end
+
+
+
 end
